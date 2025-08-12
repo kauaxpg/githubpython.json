60236424b8620a8f3f71dfb197081fd63a1130f6
kauaxpg <kauacn0@gmail.com>
2025-08-12 16:42:36 -0300

    Solução concluída do removendo duplicatas

---
+++ removendo_duplicatas.py
@@ -0,0 +1,7 @@
+numeros = [987654321, 2, 7654321, 56, 1234567, 1, 88888, 3, 42, 999999, 5, 1000000000, 13, 101010, 7, 444, 9, 2, 13, 9]
+
+def remover_duplicatas(lista=numeros):
+    return list(set(lista)) 
+
+
+print(remover_duplicatas()) 
