448ba40b4996eeb4dbb539699e585238022f00db
kauaxpg <kauacn0@gmail.com>
2025-08-12 17:13:24 -0300

    Solução concluída da invertendo lista

---
+++ invertendo_lista.py
@@ -0,0 +1,6 @@
+numeros = [987654321, 2, 7654321, 56, 1234567, 1, 88888, 3, 42, 999999, 5, 1000000000, 13, 101010, 7, 444, 9, 2, 13, 9]
+
+def inverter_lista(lista=numeros):
+    return lista[::-1] 
+
+print("Lista invertida:", inverter_lista())
