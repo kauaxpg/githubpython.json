8616ccafb640ce063caf50c9816ea5e0ebdc07bb
kauaxpg <kauacn0@gmail.com>
2025-08-12 17:04:56 -0300

    Solução concluída da calculando media

---
+++ calculando_media.py
@@ -0,0 +1,8 @@
+numeros = [987654321, 2, 7654321, 56, 1234567, 1, 88888, 3, 42, 999999, 5, 1000000000, 13, 101010, 7, 444, 9, 2, 13, 9]
+
+def calcular_media(lista=numeros):
+    soma = sum(lista)  
+    media = soma / len(lista) 
+    return media
+
+print("A média é:", calcular_media()) 
